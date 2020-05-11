d090ce53579287596692078360ce407364788f56
Gokul Sundeep <40953586+Dgs29@users.noreply.github.com>
2020-05-11 20:53:55 +0530

    Update model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -116,7 +116,7 @@ def identity_block(input_tensor, kernel_size, filters, stage, block,
     x = KL.Conv2D(nb_filter2, (kernel_size, kernel_size), padding='same',
                   name=conv_name_base + '2b', use_bias=use_bias)(x)
     x = BatchNorm(name=bn_name_base + '2b')(x, training=train_bn)
-    x = KL.Activation('relu')(x)
+    x = KL.Activation('relu', name = 'res_'+str(stage))(x)
 
     x = KL.Conv2D(nb_filter3, (1, 1), name=conv_name_base + '2c',
                   use_bias=use_bias)(x)
